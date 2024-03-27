65098e401450a87a9c7d7b35629ff372aaa0598c
maeltwosinggit <ungkuadrian@gmail.com>
2024-03-28 07:01:34 +0800

    added 3rd project

---
+++ Project 3 - iqoption/README.md
@@ -0,0 +1,48 @@
+## Project Log
+
+3/11/2023
+- tried to visualize data in Power BI (no luck haha)
+- objective candle data: to study when is the occurence of the most streak (i want to avoid that hour)
+- added manual code break function (achievable by pressing: ctrl+c)
+- added function to save the order log once break
+-* try to add - when keyboard interrupt (ctrl+c) is pressed, check current order if win<0, trade again until win (things to consider for this fx: may backfire if we want to force stop (still can simply close the python process manually easily))
+
+2/11/2023
+- pulled candle data for 1000 minutes, and 10800 mins (1 week)
+- facing issue plotting time series
+
+31/10/2023
+- testing without Iq.connect() in while loop - successful cycle 266 before not enough balance, however, it didnt trigger the not enough balance if statement. maybe because of the if statement position in the code.
+-* try to get candle info - study on candle pattern - get also info on very small candle.
+
+
+30/10/2023
+- after set sleep screen to never, successfully run 31 cycles by using infinite while loop
+- reason it breaks is due to multiple loss until not enough balance: initial balance during the time is 500$
+- currently retesting with 1000$ balance
+- Log Saved #4 - error place order at cycle 54, (note: ada usik minor things kat apps, which i dont think it will affect the script but who knows right?)
+- 54 cycle final balance = 1127.99$ yahoo!
+- retry cycle using EURUSD pair - result stuck at cycle 18
+- need to try this line - Iq.get_all_init() - study the output. Why? see below note
+- notice issue where placed order is fixed at 89% profit only eventhough apps shows 92%
+- tiba2 order at 22:06 dapat pula 92% win (pelik haha) lepastu order at 22:09 jadi 89% balik lol
+- retry cycle using EURUSD pair - result stuck at cycle 12 - rasanya sebab internet tak stable ni (my phone main ml (same network) pun ping tinggi dunno whether its related or not :P)
+- retry survived until cycle 34
+- above test contains Iq.connect() at every iteration with an average of 5-6 seconds of order placement after new candle
+
+29/10/2023
+- issue increment max range dah solve, check max range = count kena letak belah bawah
+- current issue, connection issue: run script lepas iteration ke-6 dia sangkut, check iq option apps kata timeout.
+- cuba untuk letak Iq.connect() kat bahagian while loop, tapi still sangkut kat iteration ke-6
+- retest solution atas, dgn letak indicator print connection status
+- cuba identify solution baru, set sleep screen to never (haha)
+
+
+28/10/2023
+- implement and run iqoption script
+- partial ready. ada few bugs kena cater
+- takleh guna for loop, sebab tak increment max range bila kita ada loss kat last iteration
+- boleh guna while loop, solve issue atas
+
+27/10/2023
+- study on iqoption api for python
---
+++ Project 3 - iqoption/buy_spot_martingale.py
@@ -0,0 +1,105 @@
+from iqoptionapi.stable_api import IQ_Option
+import logging
+import random
+import time
+
+import pandas as pd
+from datetime import datetime
+
+#logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(message)s')
+Iq=IQ_Option("ungkuadrian@gmail.com","Nanana123")
+Iq.connect()#connect to iqoption
+ACTIVES="EURUSD-OTC" #EURUSD-OTC
+duration=1#minute 1 or 5
+amount=5
+action="call"#put
+coef = 2.18
+cycle = 0
+
+win = 1
+max_range = 10
+count = -1
+record = []
+
+try:
+    while count < max_range:
+        print("\n\nCheck Connection:",Iq.check_connect())
+        #Iq.connect()#connect to iqoption #letak sementara untuk test connection, maybe akan issue delay time
+        #count+=1    #comment out for infinite loop
+        cycle+=1
+        print("Cycle:", cycle)    
+        print("iteration",count)
+        print("current max range:", max_range)
+        
+        print("Current Balance:",Iq.get_balance())
+
+        
+        
+        #will break once not enough amount
+        if Iq.get_balance()<amount:
+            print("Balance", Iq.get_balance(),"is lesser than required amount", amount)
+            break
+        
+        else:
+            #martingale
+            if win<0:
+                amount = amount*coef
+                print("Amount: ", amount)
+                _,id=(Iq.buy_digital_spot(ACTIVES,amount,action,duration))
+                print(_)
+                print(id)
+                print("Time Placed =", datetime.fromtimestamp(Iq.get_server_timestamp()))
+                if id !="error":
+                    while True:
+                        check,win=Iq.check_win_digital_v2(id)
+                        if check==True:
+                            break
+                    if win<0:
+                        print("you loss "+str(win)+"$")
+                    else:
+                        print("you win "+str(win)+"$")
+                else:
+                    print("please try again")
+            
+            else:
+                amount = 5
+                print("Amount: ", amount)
+                _,id=(Iq.buy_digital_spot(ACTIVES,amount,action,duration))
+                print(_)
+                print(id)
+                print("Time Placed =", datetime.fromtimestamp(Iq.get_server_timestamp()))
+                if id !="error":
+                    while True:
+                        check,win=Iq.check_win_digital_v2(id)
+                        if check==True:
+                            break
+                    if win<0:
+                        print("you loss "+str(win)+"$")
+                    else:
+                        print("you win "+str(win)+"$")
+                else:
+                    print("please try again")
+        
+        
+        #prevent close at loss for last iteration
+        if count == max_range:
+            if win<0:
+                max_range+=1
+
+        record.append([cycle,count,max_range,Iq.get_balance(),_,id,amount,datetime.fromtimestamp(Iq.get_server_timestamp()),win])
+        # record = recordappend+record
+
+
+except KeyboardInterrupt:
+    print('Loop interrupted')
+
+df = pd.DataFrame(record, columns=["Cycle", "Count", "Max Range","Current Balance", "Placeholder", "ID","Amount Placed", "Timestamp", "Win"])
+current_time = datetime.now()
+current_time_str = current_time.strftime("%Y-%m-%d %H-%M")
+
+xlsx_file = f'Log/Saved Log_{current_time_str}.xlsx'
+
+df.to_excel(xlsx_file, index=False)
+
+print(f'Data saved to {xlsx_file}')
+print(df)
